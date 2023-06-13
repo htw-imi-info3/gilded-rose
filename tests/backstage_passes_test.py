9847dff84c97a5edbb77593888a15766591b086a
abductedRhino <avocadorable@mailbox.org>
2023-06-13 17:19:18 +0200

    add test_backstage_passes_quality_never_greater_than_fifty() and fill test methods.

--- tests/backstage_passes_test.py
+++ tests/backstage_passes_test.py
@@ -13,25 +13,58 @@ Quality drops to 0 after the concert
 '''
 
 
+def test_backstage_passes_sellin_decreases_by_one_as_day_updates():
+    """All items have a SellIn value which denotes the number of days we have to sell the item"""
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == 4
+
+
 def test_backstage_passes_quality_increases_by_one_as_sellin_approaches():
-    # "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-    pass
+    """"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;"""
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 12, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.quality == 21
 
 
 def test_backstage_passes_quality_increases_by_two_ten_days_before_sellin():
-    # Quality increases by 2 when there are 10 days or less...
-    pass
+    """Quality increases by 2 when there are 10 days or less..."""
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.quality == 22
 
 
 def test_backstage_passes_quality_increases_by_three_five_days_before_sellin():
-    # ...and by 3 when there are 5 days or less but...
-    pass
+    """...and by 3 when there are 5 days or less but..."""
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.quality == 23
 
 
 def test_backstage_passes_quality_drops_to_zero_after_sellin():
-    # ...Quality drops to 0 after the concert
-    pass
+    """...Quality drops to 0 after the concert"""
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.quality == 0
 
 
 def test_backstage_passes_quality_never_negative():
-    pass
+    """The Quality of an item is never negative"""
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    gilded_rose.update_quality()
+    assert item.quality >= 0
+
+def test_backstage_passes_quality_never_greater_than_fifty():
+    """The Quality of an item is never more than 50"""
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 12, 50)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    gilded_rose.update_quality()
+    assert item.quality <= 50
