b1eac6a13308a077718b90bd7ed3a5bc2efa4e31
abductedRhino <avocadorable@mailbox.org>
2023-06-13 16:38:50 +0200

    update test_backstage_passes_*() signatures

--- tests/backstage_passes_test.py
+++ tests/backstage_passes_test.py
@@ -1,33 +1,35 @@
 from tests.settings import *
 
+'''
+- All items have a SellIn value which denotes the number of days we have to sell the item
+- All items have a Quality value which denotes how valuable the item is
 
-# create tests for normal items here...
-#	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-#	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-#	Quality drops to 0 after the concert
-
-
-def test_backstage_passes_quality_increases_as_sellin_value_approaches():
-    item = Item("name item", 5, 20)
-    gilded_rose = GildedRose([item])
-    gilded_rose.update_quality()
-    assert item.sell_in == 4
-    assert item.quality == 11
+- The Quality of an item is never negative
+- "Aged Brie" actually increases in Quality the older it gets
+- The Quality of an item is never more than 50
+- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+Quality drops to 0 after the concert
+'''
 
 
 def test_backstage_passes_quality_increases_by_one_as_sellin_approaches():
+    # "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
     pass
 
 
 def test_backstage_passes_quality_increases_by_two_ten_days_before_sellin():
+    # Quality increases by 2 when there are 10 days or less...
     pass
 
 
 def test_backstage_passes_quality_increases_by_three_five_days_before_sellin():
+    # ...and by 3 when there are 5 days or less but...
     pass
 
 
 def test_backstage_passes_quality_drops_to_zero_after_sellin():
+    # ...Quality drops to 0 after the concert
     pass
 
 
