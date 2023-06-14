a4c959430df57827af857b4ee6fc50c023e75660
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-06-14 18:32:12 +0200

    do not xfail bug fixed, remove example testcases

--- tests/settings.py
+++ tests/settings.py
@@ -3,5 +3,5 @@ import pytest
 from gilded_rose.refactored.gilded_rose import GildedRose, Item
 
 xfail_bug_in_original = True  
-xfail_bug_fix = True
+xfail_bug_fix = False
 xfail_new_features = True
