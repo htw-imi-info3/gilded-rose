3222104d187018e9952ef17090221a7a486a6172
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-07-03 21:07:18 +0200

    extract cut_after method creator

--- gilded_rose/refactored/update_item.py
+++ gilded_rose/refactored/update_item.py
@@ -31,10 +31,6 @@ def update_aged_brie(item):
 
 
 def update_backstage_pass(item):
-    if item.sell_in < 0:
-        item.quality = 0
-        return
-
     def backstage_change():
         if item.sell_in < 5:
             return 3
@@ -43,6 +39,13 @@ def update_backstage_pass(item):
         return 1
     item.quality = item.quality + backstage_change()
 
+def cut_after(update_function):
+    def cut_past_sell_in(item):
+        update_function(item)
+        if item.sell_in < 0:
+            item.quality = 0
+    return cut_past_sell_in
+     
 
 def update_sulfuras(item):
     item.quality = 80
@@ -54,7 +57,7 @@ UPDATERS = {DEFAULT_KEY: create_update_function(update_normal),
             "Conjured Item": create_update_function(update_conjured),
             "Aged Brie": create_update_function(update_aged_brie),
             "Backstage passes to a TAFKAL80ETC concert":
-                create_update_function(update_backstage_pass),
+                create_update_function(cut_after(update_backstage_pass)),
             "Sulfuras, Hand of Ragnaros": update_sulfuras
             }
 
