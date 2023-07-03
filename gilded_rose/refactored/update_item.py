75ddbcfeebe1a11db80f6a4696593c826b069cc3
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-07-03 20:56:50 +0200

    add default, remove fallback to old update strategy

--- gilded_rose/refactored/update_item.py
+++ gilded_rose/refactored/update_item.py
@@ -5,7 +5,7 @@ def create_update_function(special_updater):
             item.quality = 0
         elif item.quality > 50:
             item.quality = 50
-                
+
     def template_function(item):
         cap(item)
         item.sell_in = item.sell_in - 1
@@ -28,10 +28,12 @@ def update_aged_brie(item):
     change = 2 if item.sell_in < 0 else 1
     item.quality = item.quality + change
 
+
 def update_backstage_pass(item):
     if item.sell_in < 0:
         item.quality = 0
         return
+
     def backstage_change():
         if item.sell_in < 5:
             return 3
@@ -39,17 +41,17 @@ def update_backstage_pass(item):
             return 2
         return 1
     item.quality = item.quality + backstage_change()
-  
+
+
 def update_sulfuras(item):
     item.quality = 80
-    
-# "Normal Item"
 
-UPDATERS = {"normal with any name": create_update_function(update_normal),
+DEFAULT_KEY = "----default-----"
+UPDATERS = {DEFAULT_KEY: create_update_function(update_normal),
             "Conjured Item": create_update_function(update_conjured),
             "Aged Brie": create_update_function(update_aged_brie),
             "Backstage passes to a TAFKAL80ETC concert": create_update_function(update_backstage_pass),
-            "Sulfuras, Hand of Ragnaros": update_sulfuras    
+            "Sulfuras, Hand of Ragnaros": update_sulfuras
             }
 
 
@@ -57,7 +59,9 @@ def update(item):
     if item.name in UPDATERS.keys():
         UPDATERS[item.name](item)
     else:
-        ItemUpdaterFactory.strategy_for(item).update(item)
+        UPDATERS[DEFAULT_KEY](item)
+        # ItemUpdaterFactory.strategy_for(item).update(item)
+
 
 class ItemUpdaterFactory:
     class_list = []
